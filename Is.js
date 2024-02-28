a48a9e5ebc1dcd6fb105f48258deec8a7e4b73e3
Alaudhin936 <160020112+Alaudhin936@users.noreply.github.com>
2024-02-28 22:01:53 +0530

    Update Is.js

--- Is.js
+++ Is.js
@@ -3,9 +3,10 @@ const listItems=document.getElementById("listItems");
 menuBtn.onclick=function(){
    
 if(listItems.style.display=="none"||listItems.style.display==""){
-     listItems.style.display="block";
+       listItems.style.display="block";
+    
 }else{
     
-    listItems.style.right="none";   
+    listItems.style.display="none";   
 }
 }
